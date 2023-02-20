c4f0b1d97f02f7a660745a687528a82b3a1bbc3a
DESKTOP-3TDKCNR\PROGRAMMING <dalton_vargas@hotmail.com>
2023-02-19 21:06:39 -0500

    feat: se incorporo el estado de la cuenta de usuario para el login

--- src/main/java/com/apu_api/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/apu_api/security/JwtAuthenticationEntryPoint.java
@@ -1,11 +1,13 @@
 package com.apu_api.security;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.io.Serializable;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -19,13 +21,18 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Se
 	@Override
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 			AuthenticationException authException) throws IOException {
-			
-		if(authException.getClass() == DisabledException.class) {
-			response.sendError(HttpServletResponse.SC_FORBIDDEN, "Forbidden");
-		}else {
-			response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
-		}
 		
+		response.setContentType("application/json");
+	    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+	    PrintWriter writer = response.getWriter();
+	    
+	    if (authException instanceof BadCredentialsException) {
+	    	writer.write("{\"message\":\"Credenciales no validas\", \"error\":\"UNAUTHORIZED\"}");
+	    } else if (authException instanceof DisabledException) {
+	    	writer.write("{\"message\":\"Cuenta bloqueada\", \"error\":\"USER_BLOCKED\"}");
+	    }
+	    
+	    writer.flush();
 		
 	}
 
